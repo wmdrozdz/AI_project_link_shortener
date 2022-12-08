81f7ccade89b28e44eedb790be6ab3868e24dbcb
GaborLukjaniuk <gabor.lukjaniuk@proton.me>
2022-12-08 16:32:58 +0100

    add TranslateLinkController

---
+++ src/Controller/TranslateLinkController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\LinkRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TranslateLinkController extends AbstractController
+{
+    #[Route('{shortLink}', name: 'app_translate_link')]
+    public function index($shortLink, LinkRepository $linkRepository): Response
+    {
+        $link = $linkRepository->findOneBy(['shortLink' => $shortLink]);
+
+        // return $this->render('translate_link/index.html.twig', [
+        //     'controller_name' => $link->getLongLink(),
+        // ]);
+
+        return $this->redirect($link->getLongLink());
+    }
+}
